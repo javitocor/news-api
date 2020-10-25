72150e93b3a937b2046f9e9d61b890f069c4360f
javitocor <javitocor@gmail.com>
2020-10-25 10:22:50 +0100

    add validation for association

--- app/models/new.rb
+++ app/models/new.rb
@@ -1,6 +1,7 @@
 class New < ApplicationRecord
   belongs_to :user
-
+  validates :user, :presence => true
+  
   validates :title, presence: true, allow_blank: false, length: {minimum: 4, maximum: 25}
   validates :content, presence: true, allow_blank: false, length: {minimum: 25}
 end

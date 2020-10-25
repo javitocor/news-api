53d7b62fc25b9245cd50a4d5b1e801b9a7eb3a00
javitocor <javitocor@gmail.com>
2020-10-25 10:27:07 +0100

    create sigup and signin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do 
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
+    end
+  end
 end

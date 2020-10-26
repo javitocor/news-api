a2a476360eeade56f1fd49c7d2577928af7f000d
javitocor <javitocor@gmail.com>
2020-10-26 10:15:31 +0100

    fix issue with user controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
-require 'capybara/rspec'
 RSpec.describe User, type: :model do
   context 'User associations tests' do
-    it { should have_many(:new) }
+    it { should have_many(:news) }
   end
 end
